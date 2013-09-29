1a7e0a8cf51c5066e3679427821d11cd144af5b5
Linkyu <linkyutl@gmail.com>
2013-09-29 09:37:01 +0200

    Unity content
    uploaded the most related assets to date.
    hexa_cylinder(.obj/.blend) is an hexagonal cylinder; the basic bloc that
    will be used.
    Grid.cs and playerManager.cs are scripts used in the current test build,
    although they're mere tests too.

---
+++ Grid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public Rigidbody bloc;
+	public Transform origin;
+	public int length;
+	public int width;
+	
+	private Vector3[] gridPos;
+	
+	void Start () 
+	{
+		for(float i = 0 ; i<length ; i++)
+		{
+			for(float j = 0 ; j<width ; j++)
+			{
+				if (i%2==0)
+				{
+					Instantiate (bloc, new Vector3(j-j*(1-Mathf.Sqrt(3)/2), 1, i-i*0.25f), origin.rotation);
+				}
+				else
+				{
+					Instantiate (bloc, new Vector3(j+Mathf.Sqrt(3)/4-j*(1-Mathf.Sqrt(3)/2), 1, i-i*0.25f), origin.rotation);
+				}
+			}
+		}
+		
+	}
+}
---
+++ playerManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerManager : MonoBehaviour {
+	
+	public Rigidbody model;
+	public Transform spawn;
+	public float moveSpeed;
+	public float jumpForce;
+	
+	private Rigidbody player;
+	
+	// Use this for initialization
+	void Start () {
+		player = Instantiate(model, spawn.position, spawn.rotation) as Rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+		float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+		
+		player.transform.Translate(new Vector3(v, 0, -h));
+		
+		if (Input.GetButtonDown("Jump"))
+		{
+			player.AddForce(Vector3.up * jumpForce); //, ForceMode.Acceleration, jumpForce, ForceMode.Acceleration);
+			Debug.Log("Jump!");
+		}
+	}
+}
